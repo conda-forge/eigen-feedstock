d585ad0f175af6fd8ec704697099b7764efeb1e8
seanyen <seanyen@corp.microsoft.com>
2019-10-10 18:22:55 -0700

    Generate pkg-config files for Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,7 @@ cmake -LAH -G"NMake Makefiles"              ^
   -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%         ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%      ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%   ^
+  -DEIGEN_BUILD_PKGCONFIG=ON ^
   ..
 if errorlevel 1 exit 1
 
